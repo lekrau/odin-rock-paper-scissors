368cd8797b0e064e3db43ffce1a2fd87ae03c425
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-07 12:11:59 +0200

    Set up rock paper scissors project

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # odin-rock-paper-scissors
+
+Project: Rock Paper Scissors
+For this project, you will create the game Rock Paper Scissors. This game will be played entirely in the console.
+
+From the TOP Lesson [Project: Rock Paper Scissors](https://www.theodinproject.com/lessons/foundations-rock-paper-scissors)
+
+Goal: Implement rock paper scissors as a JS CLI game.
---
+++ script.js
@@ -0,0 +1,38 @@
+// Problem 1: Write a function that randomly returns “rock”, “paper” or “scissors”.
+
+// Problem Description in My Own Words
+// 
+
+// Input: TBD
+// Output: TBD
+
+// Assumptions
+// - TBD
+
+// Test Cases (2 - 4)
+// Normal case:
+// FUNCTION(ARGUMENTS) -> RESULT
+// Edge / boundary case:
+// FUNCTION(ARGUMENTS) -> RESULT
+// OPTIONAL Case related to assumptions:
+// FUNCTION(ARGUMENTS) -> RESULT
+// OPTIONAL Out of scope:
+// FUNCTION(ARGUMENTS) -> no valid result
+
+// Rough Plan / Subproblems
+// - TBD
+// Next Step (Smallest Subproblem): TBD
+
+// Pseudocode for the Next Step
+// TBD
+
+// Implementation
+"use strict";
+
+console.log("Hello World")
+// TBD
+
+// Short Debug Log
+// What went wrong, if anything?
+// Which assumption was tested or confirmed?
+// What did I change or learn?
