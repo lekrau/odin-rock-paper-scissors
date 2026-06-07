1200c319e9bc5565dc9a26382f3d153c53c17142
lekrau <206306997+lekrau@users.noreply.github.com>
2026-06-07 13:41:50 +0200

    Add computer choice logic

--- script.js
+++ script.js
@@ -1,36 +1,50 @@
-// Problem 1: Write a function that randomly returns “rock”, “paper” or “scissors”.
+// Problem 1: Write the logic to get the computer choice
 
 // Problem Description in My Own Words
-// 
+// Write a function that randomly returns “rock”, “paper” or “scissors”.
 
-// Input: TBD
-// Output: TBD
+// Input: -
+// Output: choice of the computer (string)
 
 // Assumptions
-// - TBD
+// - The computer choses randomly
+// - Each choice has the same probability (within reason)
 
 // Test Cases (2 - 4)
 // Normal case:
-// FUNCTION(ARGUMENTS) -> RESULT
-// Edge / boundary case:
-// FUNCTION(ARGUMENTS) -> RESULT
-// OPTIONAL Case related to assumptions:
-// FUNCTION(ARGUMENTS) -> RESULT
-// OPTIONAL Out of scope:
-// FUNCTION(ARGUMENTS) -> no valid result
+// getComputerChoice() -> "rock" || "paper" || "scissors"
 
 // Rough Plan / Subproblems
-// - TBD
-// Next Step (Smallest Subproblem): TBD
+// - Generate a random number
+// - If it is low, return rock
+// - If it is medium, return paper
+// - If it is high, return scissors
+// Next Step (Smallest Subproblem): No need to add unnecessary complexity (helper functions)
 
 // Pseudocode for the Next Step
-// TBD
+// Create a variable called randomNumber of type number that starts with the a random value between 0 and 1.
+// If randomNumber is < 1/3
+    // return rock
+// Else if randomNumber is < 2/3
+    // return paper
+// Else
+    // return scissors
 
 // Implementation
 "use strict";
 
-console.log("Hello World")
-// TBD
+function getComputerChoice(){
+    let randomNumber = Math.random();
+    if (randomNumber < (1 / 3)) {
+        return "rock";
+    } else if (randomNumber < (2 / 3)) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+// console.log("getComputerChoice()", getComputerChoice());
 
 // Short Debug Log
 // What went wrong, if anything?
